e682cb8fe31f0dce8d037741549404e463883382
Kamal <kamalfarhat@gmail.com>
2023-09-11 22:01:37 +0800

    commit re-former project

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+    def new
+        @user = User.new
+
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to new_users_path
+        else
+            render :new, :unprocessable_entity
+        end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+    
+        if @user.update(user_params)
+          redirect_to @user
+        else
+          render :edit, status: :unprocessable_entity
+        end
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :username, :password)
+    end
+end
+
